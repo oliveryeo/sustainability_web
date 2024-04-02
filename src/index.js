91fd713d5c6c6c0b1f0f7fbdd8377275763a5e44
oliveryjy <yjyoliver@hotmail.com>
2024-04-02 22:37:00 +0800

    Update index.js to load homepage logo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles.css';
+import SunflowerLogo from './assets/flower.svg';
+
+// <img src="./assets/flower.svg" alt="flower-icon">
+
+// Logo for homepage title
+const logoTitleBtn = document.querySelector(".logo-title-btn");
+const logoTitle = document.querySelector(".logo-title-btn > div");
+const sunflowerLogo = new Image();
+sunflowerLogo.src = SunflowerLogo;
+sunflowerLogo.alt = "flower-icon";
+logoTitleBtn.insertBefore(sunflowerLogo, logoTitle);
+
+console.log(logoTitleBtn);
