c6bdd8da0e6cdaf3d67748d2893bfe7eafc9fd9d
oliveryjy <yjyoliver@hotmail.com>
2024-04-08 19:12:06 +0800

    Create skeleton for dropdown menu
    Navbar is created with a draft dropdown menu. Dropdown menu was created
    with a div element because a button restricts creativity of adding
    items like arrow icons.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './styles/reset.css';
 import './styles/main.css';
 import sunflowerLogoImg from './assets/flower.svg';
+import triangleImg from './assets/triangle-down.svg';
 
 // Logo for homepage title
 const pageLogo = document.querySelector("#page-logo");
 pageLogo.src = sunflowerLogoImg;
 pageLogo.alt = "flower-icon";
+
+// Logo for dropdown menu
+const dropdownMenuIcon = document.querySelector("#help-dropdown > img");
+dropdownMenuIcon.src = triangleImg;
