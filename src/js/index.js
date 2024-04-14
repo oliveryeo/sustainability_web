803b7ca00060b508a0e30a02c999b7a1ea9128d4
oliveryjy <yjyoliver@hotmail.com>
2024-04-14 21:50:53 +0800

    Update skeleton for image slider
    Also renamed css files for clarity

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../styles/reset.css';
-import '../styles/main.css';
+import '../styles/index.css';
 import sunflowerLogoImg from '../assets/flower.svg';
 import triangleImg from '../assets/triangle-down.svg';
 import { runDropdownMenuEvents } from './dropdownMenu';
@@ -13,6 +13,12 @@ pageLogo.alt = "flower-icon";
 const dropdownMenuIcon = document.querySelector("#help-dropdown > img");
 dropdownMenuIcon.src = triangleImg;
 dropdownMenuIcon.alt = "dropdown triangle";
+
+// Images for slide show
+const slideImageOne = document.getElementById("image-slider-1");
+const slideImageTwo = document.getElementById("image-slider-2");
+const slideImageThree = document.getElementById("image-slider-3");
+const slideImageFour = document.getElementById("image-slider-4");
 
 // Run dropdown menu events
 runDropdownMenuEvents();
