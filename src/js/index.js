52a43d5058f3349a606342d447fc9c172ad58271
oliveryjy <yjyoliver@hotmail.com>
2024-04-16 22:53:00 +0800

    Add function to change slide on indicator clicks
    Changed setTimeout to setInterval as there were issues with setTimeout
    stopping even after setting timeout again after clearTimeout
    
    Refactored the showSlides() function, repurposing it to just load the
    WITHOUT messing with the slideIndex variable.
    
    setInterval function is now separated into the main body so that
    eventListeners for the slide arrows and indicators can reference it
    to reset.
    
    Slide arrows and indicators is now able to change the slides by
    modifying the slideIndex variable and utilizing the showSlides()
    function after.
    
    changeSlides() function is added to handle the usual logic of modifying
    the slideIndex variable upon invokation. This function is used in the
    setInterval function.

--- src/js/index.js
+++ src/js/index.js
@@ -8,6 +8,9 @@ import slideArrowLeftImg from "../assets/slideshow-left-arrow.svg";
 import slideArrowRightImg from "../assets/slideshow-right-arrow.svg";
 import { runDropdownMenuEvents } from "./dropdownMenu";
 
+/* 
+  *****************LOGIC FOR PAGE INITIATION*****************
+*/
 // Logo for homepage title - Can probably separate this into a separate navbar javascript file
 const pageLogo = document.querySelector("#page-logo");
 pageLogo.src = sunflowerLogoImg;
@@ -26,13 +29,15 @@ const slideImageOne = document.querySelector("#image-slider-1 > img");
 slideImageOne.src = slideImgOne;
 slideImageOne.alt = "Beach shores becoming a garbage magnet for plastic waste";
 const captionOne = document.querySelector("#image-slider-1 > figcaption");
-captionOne.textContent = "Beach shores becoming a garbage magnet for plastic waste";
+captionOne.textContent =
+  "Beach shores becoming a garbage magnet for plastic waste";
 
 const slideImageTwo = document.querySelector("#image-slider-2 > img");
 slideImageTwo.src = slideImgTwo;
 slideImageTwo.alt = "Sea turtles mistake plastic waste as jellyfish for food";
 const captionTwo = document.querySelector("#image-slider-2 > figcaption");
-captionTwo.textContent = "Sea turtles mistake plastic waste as jellyfish for food";
+captionTwo.textContent =
+  "Sea turtles mistake plastic waste as jellyfish for food";
 
 const slideImageThree = document.querySelector("#image-slider-3 > img");
 slideImageThree.src = slideImgThree;
@@ -49,21 +54,97 @@ const slideArrowRight = document.querySelector(".carousel-right-arrow");
 slideArrowRight.src = slideArrowRightImg;
 slideArrowRight.alt = "Right Arrow for slide change";
 
-// Logic for carousel
+/*
+  *****************LOGIC FOR CAROUSEL SLIDES*****************
+*/
 let slideIndex = 1;
 const allSlides = document.querySelectorAll(".carousel-slide > div");
+const allIndicators = document.querySelectorAll(
+  ".carousel-slide-indicator > span"
+);
+const slideArrows = document.querySelectorAll("#carousel > img");
+
+// Show slides once
 showSlides();
+
+// Change slide every 4 seconds
+let slideTimer = setInterval(changeSlides, 4000);
+
+// Add event listener for each indicator
+allIndicators.forEach((indicator) => {
+  indicator.addEventListener("click", () => {
+    // Retrieve the slide number and set it as the slideIndex
+    let slideNumber = indicator.dataset.value;
+    slideIndex = slideNumber;
+
+    // Show the slides
+    showSlides();
+
+    // Reset timer
+    clearInterval(slideTimer);
+    slideTimer = setInterval(changeSlides, 4000);
+  });
+});
+
+// Add event listener for the slide arrows
+slideArrows.forEach((arrow) => {
+  arrow.addEventListener("click", () => {
+    let slideCommand = arrow.dataset.value;
+
+    // Go back one slide
+    if (slideCommand == "left") {
+      slideIndex--;
+      console.log(slideIndex);
+      
+      // If slideIndex reaches bottom threshold, set the index to the highest slide number
+      if (slideIndex < 1) {
+        slideIndex = allSlides.length;
+      }
+      
+      // Show the slides and reset the timer
+      showSlides();
+      clearInterval(slideTimer);
+      slideTimer = setInterval(changeSlides, 4000);
+    }
+
+    // Go forward one slide
+    if (slideCommand == "right") {
+      slideIndex++;
+      
+      // If slideIndex reaches top threshold, set the index to the lowest slide number
+      if (slideIndex > allSlides.length) {
+        slideIndex = 1;
+      }
+      
+      // Show the slides and reset the timer
+      showSlides();
+      clearInterval(slideTimer);
+      slideTimer = setInterval(changeSlides, 4000);
+    }
+  })
+})
 
 function showSlides() {
   // Hide all slides
-  allSlides.forEach(slide => {
+  allSlides.forEach((slide) => {
     slide.style.display = "none";
+  });
+
+  // Reset all indicators from being selected
+  allIndicators.forEach((indicator) => {
+    indicator.style.backgroundColor = "";
   });
 
   // Identify the slide to display
   const slideToDisplay = allSlides[slideIndex - 1];
   slideToDisplay.style.display = "block";
 
+  // Identify the indicator to select
+  const selectedIndicator = allIndicators[slideIndex - 1];
+  selectedIndicator.style.backgroundColor = "darkgray";
+}
+
+function changeSlides() {
   // Add the counter for the next slide
   slideIndex++;
 
@@ -71,10 +152,7 @@ function showSlides() {
   if (slideIndex > allSlides.length) {
     slideIndex = 1;
   }
-  
-  // Change slide every 4 seconds
-  setTimeout(showSlides, 4000);
-  
+
+  // Show the slide
+  showSlides();
 }
-
-
